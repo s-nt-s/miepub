77fcb66c9605f9159ee2452cef53b98fe1a95baf
santos82 <santos82h@gmail.com>
2016-02-01 21:00:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Script sencillo para generar epub a partir de markdown sorteando algunos bugs de pandoc.
---
+++ miebup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SCP=$(dirname "$0")
+DIR=$(pwd)
+MD=`ls *.md | head -n 1`
+if [ -z "$MD" ]; then
+	echo "Markdown no encontrado"
+	exit 1
+fi
+echo "Convirtiendo $MD"
+
+EPUB=`echo $MD | sed 's/md$/epub/'`
+
+TMP=$(mktemp -d)
+
+cp "$MD" "$TMP"
+
+TMD="$TMP/$MD"
+sed -r 's/^([\t ]*)[ivxdlcm]+\. /\1#. /i' -i "$TMD"
+sed -r 's/^([\t ]*[A-Z])\. /\1) /i' -i "$TMD"
+sed -r 's/^([1-9]+[0-9]*)\) /\1\\) /' -i "$TMD"
+
+if [ -f "~/.pandoc/epub.css" ]; then
+	cp ~/.pandoc/epub.css ~/.pandoc/epub.css.bak
+fi
+
+if [ -f "$DIR/epub.css" ]; then
+	cp "$DIR/epub.css" ~/.pandoc/
+elif [ -f "$SCP/epub.css" ]; then
+	cp "$SCP/epub.css" ~/.pandoc/
+fi
+
+pandoc -S --from markdown+ascii_identifiers -o "$TMP/$EPUB" "$TMD"
+
+if [ -f "~/.pandoc/epub.css.bak" ]; then
+	cp ~/.pandoc/epub.css.bak ~/.pandoc/epub.css
+	rm ~/.pandoc/epub.css.bak
+fi
+
+cd "$TMP"
+
+rm "$MD"
+
+unzip -q "$EPUB"
+
+rm "$EPUB"
+
+rm nav.xhtml
+rm title_page.xhtml
+
+sed '/<item id="nav" /d' -i content.opf
+sed '/<item id="title_page" /d' -i content.opf
+sed '/<itemref idref="title_page" /d' -i content.opf
+sed '/<itemref idref="nav" /d' -i content.opf
+sed '/href="nav.xhtml"/d' -i content.opf
+perl -0777 -pe 's/\s*<navPoint id=.navPoint-0.>\s*<navLabel>\s*<text>.*?\s*<\/navLabel>\s*<content src="title_page.xhtml" \/>\s*<\/navPoint>//igs' -i toc.ncx  
+zip -r -q "$EPUB" *
+cp "$EPUB" "$DIR/$EPUB"
+
