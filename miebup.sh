10687f76b241e489f4507230a939ffa71293e31f
santos82 <santos82h@gmail.com>
2016-02-12 00:24:35 +0100

    mover notas al final

--- miebup.sh
+++ miebup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCP=$(dirname "$0")
+SCP=$(dirname $(realpath "$0"))
 DIR=$(pwd)
 MD=`ls *.md | head -n 1`
 if [ -z "$MD" ]; then
@@ -20,6 +20,16 @@ sed -r 's/^([\t ]*)[ivxdlcm]+\. /\1#. /i' -i "$TMD"
 sed -r 's/^([\t ]*[A-Z])\. /\1) /i' -i "$TMD"
 sed -r 's/^([1-9]+[0-9]*)\) /\1\\) /' -i "$TMD"
 
+NT=0
+if grep --quiet "\[\^1\]" "$TMD"; then
+	NT=1
+	if ! grep --quiet "^# Notas" "$TMD"; then
+		echo "" >> "$TMD"
+		echo "# Notas" >> "$TMD"
+		echo "" >> "$TMD"
+	fi
+fi
+
 if [ -f "~/.pandoc/epub.css" ]; then
 	cp ~/.pandoc/epub.css ~/.pandoc/epub.css.bak
 fi
@@ -30,6 +40,7 @@ elif [ -f "$SCP/epub.css" ]; then
 	cp "$SCP/epub.css" ~/.pandoc/
 fi
 
+echo "Ejecutando pandoc"
 pandoc -S --from markdown+ascii_identifiers -o "$TMP/$EPUB" "$TMD"
 
 if [ -f "~/.pandoc/epub.css.bak" ]; then
@@ -53,7 +64,12 @@ sed '/<item id="title_page" /d' -i content.opf
 sed '/<itemref idref="title_page" /d' -i content.opf
 sed '/<itemref idref="nav" /d' -i content.opf
 sed '/href="nav.xhtml"/d' -i content.opf
-perl -0777 -pe 's/\s*<navPoint id=.navPoint-0.>\s*<navLabel>\s*<text>.*?\s*<\/navLabel>\s*<content src="title_page.xhtml" \/>\s*<\/navPoint>//igs' -i toc.ncx  
+perl -0777 -pe 's/\s*<navPoint id=.navPoint-0.>\s*<navLabel>\s*<text>.*?\s*<\/navLabel>\s*<content src="title_page.xhtml" \/>\s*<\/navPoint>//igs' -i toc.ncx
+
+if [ $NT -eq 1 ]; then
+	echo "Generando notas"
+	python "$SCP/notas.py"
+fi
+
 zip -r -q "$EPUB" *
 cp "$EPUB" "$DIR/$EPUB"
-
